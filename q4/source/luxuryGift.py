a1a675e4fc5bd65602af16e4ae45ac7b42b681ad
Nidheesh Pandey <nidheeshpandey@gmail.com>
2017-04-09 20:03:42 +0530

    Add files via upload

--- q4/source/luxuryGift.py
+++ q4/source/luxuryGift.py
@@ -2,13 +2,11 @@ from gift import Gift
 
 class Luxury(Gift) :
 	'defines a luxury gift'
-
-	luxrating = 0
-	"""@ivar: luxury rating."""
-	diff = 0
-	"""@ivar: difficulty to obtain gift."""
+	
 	def __init__(self,name,price,value,types,luxrating,diff):
 		'initialises attributes of Luxury class'
 		Gift.__init__(self,name,price,value,types)
 		self.luxrating = luxrating
+		"""@ivar: luxury rating."""
 		self.diff = diff
+		"""@ivar: difficulty to obtain gift."""
